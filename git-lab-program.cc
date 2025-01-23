56f777a8f14baf300171c60e4853b1fdaa659430
Jimmy-cs01 <jimmyjr0616@gmail.com>
2025-01-22 19:29:53 -0500

    Done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jimmy Muhly
+ *        Date: 01-22-2025
+ * Description: Lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
